14e0df65329752a8780ff445e6e92a5e4286f847
triCoffeeMilk <dodo100834@gmail.com>
2022-11-30 23:15:45 +0900

    create nuxt project

---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Nuxt Movie App',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Roboto:wght@400;700&display=swap' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/css/main.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '~/plugins'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/style-resources',
+    '@nuxtjs/dotenv'
+  ],
+
+  styleResources: {
+    scss: [
+      '~/scss/main.scss'
+    ]
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    babel: {
+      presets: ['@babel/preset-env'],
+      plugins: [
+        ['@babel/plugin-transform-runtime']
+      ]
+    },
+    postcss: {
+      plugins: [
+        require('autoprefixer')
+      ]
+    }
+  },
+
+  serverMiddleware: [
+    {
+      path: '/api/movie',
+      handler: '~/server-middleware/movie.js'
+    }
+  ]
+}
---
+++ store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'JEAN',
+    email: 'dodo100834@gmail.com',
+    blog: 'https://cloud30.tistory.com/',
+    phone: '+82-10-1234-5678',
+    image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FekgaC7%2FbtrSahorCHs%2FWMz9jRY8lHtkr8v1vKLYt0%2Fimg.png'
+  })
+}
